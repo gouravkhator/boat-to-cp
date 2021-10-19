f4037bc1df5f88884cfb4c2be32ecf2e2b8cfbeb
Gourav Khator <gouravkhator9@gmail.com>
2021-10-19 12:47:18 +0530

    Solved combination sum leetcode

--- LeetCode_and_BinarySearch/CombinationSum_bestApproach.java
+++ LeetCode_and_BinarySearch/CombinationSum_bestApproach.java
@@ -1,37 +1,47 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
-class CombinationSum_best {
+class CombinationSum {
     // Problem Question: https://leetcode.com/problems/combination-sum/
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> combinations = new ArrayList<List<Integer>>();
-        int length = candidates.length;
-        Arrays.sort(candidates);
-        List<Integer> temp = new ArrayList<Integer>();
-        getCombinations(combinations,candidates,temp,target,length,0);
-        return combinations;
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        recur(target, candidates, 0, new ArrayList<>(), result);
+        return result;
     }
-    
-    void getCombinations(List<List<Integer>> combinations,int[] arr,List<Integer> temp,int remaining,int n,int index)
-    {
-        if(remaining<0)
-            return;
-        if(remaining == 0)
-        {
-            // nothing remaining so add the list to list of lists
-            List<Integer> res = new ArrayList<Integer>(temp);
-            combinations.add(res);
+	
+	static void recur(int remainingSum, int[] nums, int startIndex, ArrayList<Integer> list, List<List<Integer>> result) {
+        /*
+        Logic:
+
+        If remaining sum is negative, just return.
+
+        If remaining sum is 0, that means create a new list out of that temp list (as that temp list will get modified afterwards too..)
+        Add that new list to result.
+
+        As we need duplications of same element, we start from 0 to full length and take every number.
+        For each number, we take that number into account in the list, and then recur from same index in which that number is present.
+
+        If we were to take each number at most once, we would have recurred with startIndex as i+1.
+        This means, now recur from next element.
+        
+        Then, we remove that number from list, which means we exclude that occurrence of that number.
+        Thus, we get duplicate occurrences of same number.
+        */
+
+        if(remainingSum < 0){
             return;
         }
-        for(int i=index;i<n;i++)
-        {
-            if(remaining<arr[i])
-                break;
-            // add the current element to temp list
-            temp.add(arr[i]);
-            getCombinations(combinations,arr,temp,remaining-arr[i],n,i); // get combinations for current temp list
-            temp.remove(Integer.valueOf(arr[i])); // for taking all combinations, remove the current element
+        
+        if(remainingSum == 0){
+            result.add(new ArrayList<>(list));
+            list = new ArrayList<>();
+            return;
         }
-    }
+        
+		for(int i=startIndex; i<nums.length; i++){
+            list.add(nums[i]);
+            recur(remainingSum - nums[i], nums, i, list, result);
+            list.remove(list.size() - 1);
+        }
+	}
 }
