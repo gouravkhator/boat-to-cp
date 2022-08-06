a29a73cde439ce8a53fa13cdd3a52ae2c8ddc201
Gourav Khator <gouravkhator9@gmail.com>
2022-08-06 19:14:03 +0530

    Added leetcode template for rust

---
+++ leetcode_template_rust.rs
@@ -0,0 +1,23 @@
+/**
+ * ----Analogy-----:
+ * 
+ * ----Approach----:
+ * 
+ * ----Time Complexity & Space Complexity-------:
+ * 
+ */
+pub struct Solution {}
+
+// Problem Question: __PROBLEM_LINK__
+
+// -----submission codes start here-----
+
+// -----submission codes end-----
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_problem_title_() {}
+}
